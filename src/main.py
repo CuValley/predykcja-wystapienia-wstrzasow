a0073766cb508c465654d6f4c71684a2360000a3
Kinga Głąbińska <kingagla@gmail.com>
2021-06-12 23:55:42 +0200

    wprowadzenie thresholdu

--- src/main.py
+++ src/main.py
@@ -1,5 +1,9 @@
-import pandas as pd
-import sklearn
-import matplotlib
-import numpy
-df = pd.read_excel('data/2016-2020.xlsx')
+# from src.utils import *
+
+flag = True
+while flag:
+    try:
+        threshold = int(input("Wskaż liczbę Joule'i powyżej ilu wstrząs zostanie uznany za mocny (domyślna wartość wynosi 10_000): "))
+        flag = False
+    except ValueError:
+        print('Wprowadzona wartość jest nieprawidłowa. Spróbuj jeszcze raz.')
